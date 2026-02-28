cb935a3054dac1b956f9eb3cd9621d7ab2500136
Phil de Joux <philderbeast@gmail.com>
2026-02-28 14:54:54 -0500

    ...

--- project-versions.mk
+++ project-versions.mk
@@ -1,17 +1,15 @@
 # Versions of GHC and stackage resolver, the ones we're on and the next ones
 # we're upgrading to.
-GHC_VERSION ?= 9.12.2
-STACKAGE_VERSION ?= nightly-2025-12-30
-# GHC_VERSION ?= 9.10.3
-# STACKAGE_VERSION ?= lts-24.32
+GHC_VERSION ?= 9.10.3
+STACKAGE_VERSION ?= lts-24.32
 # GHC_VERSION ?= 9.8.4
 # STACKAGE_VERSION ?= lts-23.28
 # GHC_VERSION ?= 9.6.7
 # STACKAGE_VERSION ?= lts-22.44
 
 # For the upgrade, pick a matching pair of ghc-version and stack resolver.
-GHC_UPGRADE ?= 9.6.7
-STACKAGE_UPGRADE ?= lts-22.44
+GHC_UPGRADE ?= 9.12.2
+STACKAGE_UPGRADE ?= nightly-2025-12-30
 
 # Imports can be relative to the project or relative to importing file.
 # ImportRelative works with cabal-3.10 and is the default.
